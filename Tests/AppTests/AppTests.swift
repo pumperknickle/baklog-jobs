fb4d1cb48bb4e5019db3a6b490b4d60d3f28352f
Joseph Bao <jbao@hubspot.com>
2024-08-11 16:31:42 -0500

    refactor

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -25,44 +25,57 @@ final class AppTests: XCTestCase {
     }
     
     func testTodoIndex() async throws {
-        let sampleTodos = [Todo(title: "sample1"), Todo(title: "sample2")]
+        let sampleTodos = [BaklogJob(spec: "sample1"), BaklogJob(spec: "sample2")]
         try await sampleTodos.create(on: self.app.db)
         
-        try await self.app.test(.GET, "todos", afterResponse: { res async throws in
+        try await self.app.test(.GET, "jobs", afterResponse: { res async throws in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(
-                try res.content.decode([TodoDTO].self).sorted(by: { $0.title ?? "" < $1.title ?? "" }),
-                sampleTodos.map { $0.toDTO() }.sorted(by: { $0.title ?? "" < $1.title ?? "" })
+                try res.content.decode([BaklogJobDTO].self).sorted(by: { $0.spec ?? "" < $1.spec ?? "" }),
+                sampleTodos.map { $0.toDTO() }.sorted(by: { $0.spec ?? "" < $1.spec ?? "" })
+            )
+        })
+    }
+    
+    func testTodoGet() async throws {
+        let sampleTodos = [BaklogJob(spec: "sample1"), BaklogJob(spec: "sample2")]
+        try await sampleTodos.create(on: self.app.db)
+        
+        try await self.app.test(.GET, "jobs", afterResponse: { res async throws in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(
+                try res.content.decode([BaklogJobDTO].self).sorted(by: { $0.spec ?? "" < $1.spec ?? "" }),
+                sampleTodos.map { $0.toDTO() }.sorted(by: { $0.spec ?? "" < $1.spec ?? "" })
             )
         })
     }
     
     func testTodoCreate() async throws {
-        let newDTO = TodoDTO(id: nil, title: "test")
+        let newDTO = BaklogJobDTO(id: nil, spec: "test")
         
-        try await self.app.test(.POST, "todos", beforeRequest: { req in
+        try await self.app.test(.POST, "jobs", beforeRequest: { req in
             try req.content.encode(newDTO)
         }, afterResponse: { res async throws in
             XCTAssertEqual(res.status, .ok)
-            let models = try await Todo.query(on: self.app.db).all()
-            XCTAssertEqual(models.map { $0.toDTO().title }, [newDTO.title])
+            let models = try await BaklogJob.query(on: self.app.db).all()
+            XCTAssertEqual(models.map { $0.toDTO().spec }, [newDTO.spec])
         })
     }
     
     func testTodoDelete() async throws {
-        let testTodos = [Todo(title: "test1"), Todo(title: "test2")]
+        let testTodos = [BaklogJob(spec: "jobs"), BaklogJob(spec: "test2")]
         try await testTodos.create(on: app.db)
         
-        try await self.app.test(.DELETE, "todos/\(testTodos[0].requireID())", afterResponse: { res async throws in
+        try await self.app.test(.DELETE, "jobs/\(testTodos[0].requireID())", afterResponse: { res async throws in
             XCTAssertEqual(res.status, .noContent)
-            let model = try await Todo.find(testTodos[0].id, on: self.app.db)
+            let model = try await BaklogJob.find(testTodos[0].id, on: self.app.db)
             XCTAssertNil(model)
         })
     }
 }
 
-extension TodoDTO: Equatable {
+extension BaklogJobDTO: Equatable {
     public static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.id == rhs.id && lhs.title == rhs.title
+        lhs.id == rhs.id && lhs.spec == rhs.spec
     }
 }
